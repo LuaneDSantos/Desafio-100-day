49bb90d52318a50353af9984a644c1c70b68e749
Luane Domingos dos Santos <123187344+LuaneDSantos@users.noreply.github.com>
2024-02-22 21:12:22 -0300

    Add files via upload

---
+++ day-1.py
@@ -0,0 +1,2 @@
+print ('Hi, people from Linkedin and Gihub, I come to present my day one, of a hundred days of challenge, coding in Python, so.... ')
+print ('Hello World!')
---
+++ day-2.py
@@ -0,0 +1,7 @@
+#Inserir valores inteiros no A e B
+A = 10
+B = 9
+#Somar A e B 
+X = A + B
+#Informar resultado da soma sendo o resultado de X
+print("X =" ,X)
